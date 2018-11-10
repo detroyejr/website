814d819f465b8aa345fd3f9485ef9f861b93707e
Jonathan De Troye <detroyejr@outlook.com>
2018-11-10 17:13:44 -0500

    Docstrings

--- app/utilities.py
+++ app/utilities.py
@@ -5,12 +5,15 @@ Utilities
 import os
 
 def parse_date(x):
+    """get the date from a file name"""
     return x[:10].replace("_", "-")
 
 def parse_title(x):
+    """get the title from a file name"""
     return x[11:][:-5].replace("_", " ")
 
 def generate_html_posts():
+    """create a list of articles that are passed into flask"""
     posts_list = os.listdir("app/posts")
     md_to_html()
     return [
@@ -18,5 +21,6 @@ def generate_html_posts():
     ]
 
 def md_to_html():
+    """use pandoc to convert markdown to html"""
     for file in os.listdir("/app/posts"):
         os.system("pandoc --highlight-style pygments -f markdown -t html /app/posts/{} -o /app/posts/{}".format(file, file))
