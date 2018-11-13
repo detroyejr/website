dfc89674d620ca68f30adfdcc1f1f17a9d7e3172
Jonathan De Troye <detroyejr@outlook.com>
2018-11-12 21:24:56 -0500

    Lint

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ Website App
 Description
 -----------
 The personal website of Jonathan De Troye
-
 """
+
 import os
 from flask import Flask, render_template, send_from_directory
 import pypandoc
@@ -13,18 +13,22 @@ from utilities import parse_date, parse_title, generate_html_posts
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     posts = generate_html_posts()
     return render_template("index.html", posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/images/<file>")
 def images(file):
     return send_from_directory("images", file)
+
 
 @app.route("/blog")
 def blog():
@@ -33,11 +37,12 @@ def blog():
     # generate_html_posts()
     return render_template("blog.html", posts=posts)
 
+
 @app.route("/blog/<path>")
 def article(path):
     p = "".join([x for x in open("app/posts/" + path + ".html").readlines()])
     return render_template("article.html", article_html=p)
 
-        
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- app/utilities.py
+++ app/utilities.py
@@ -4,17 +4,18 @@ Utilities
 
 import os
 
+
 def parse_date(x):
     """get the date from a file name"""
     return x[:10].replace("_", "-")
+
 
 def parse_title(x):
     """get the title from a file name"""
     return x[11:][:-5].replace("_", " ")
 
+
 def generate_html_posts():
     """create a list of articles that are passed into flask"""
     posts_list = os.listdir("app/posts")
-    return [
-        (parse_date(x), parse_title(x), x[:-5]) for x in posts_list
-    ]
+    return [(parse_date(x), parse_title(x), x[:-5]) for x in posts_list]
